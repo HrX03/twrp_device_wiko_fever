35806e9e964239233eb5583d52a01ccd0511ba14
mohancm100 <mohanmanjappa@gmail.com>
2018-01-22 06:32:38 +0000

    use MALLOC_SVELTE := true for low memory devices * MALLOC_IMPL is depricated
    Signed-off-by: mohancm100 <mohanmanjappa@gmail.com>

--- board/device.mk
+++ board/device.mk
@@ -7,4 +7,5 @@ TARGET_OTA_ASSERT_DEVICE := A7010a48,k5fpr
 USE_MINIKIN := true
 
 #Use dlmalloc instead of jemalloc for mallocs
-MALLOC_IMPL := dlmalloc
+#MALLOC_IMPL := dlmalloc
+MALLOC_SVELTE := true
